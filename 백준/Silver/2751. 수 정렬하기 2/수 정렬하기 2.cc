f95b19faefc9c75257559abe5e1fc562ab2761c4
stw0218 <39959351+stw0218@users.noreply.github.com>
2025-01-31 03:28:16 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 248 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -1,30 +1,31 @@
 #include <iostream>
 #include <vector>
-#include <set>
 #include <algorithm>
-#include <queue>
 using namespace std;
 
 #define endl '\n'
 
 int main()
 {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
 	int n;
 	cin >> n;
-
-	priority_queue<int, vector<int>, greater<int>> pq;
+	
+	vector<int> v(n);
 
 	for (int i = 0; i < n; i++)
 	{
-		int temp;
-		cin >> temp;
-		pq.push(temp);
+		cin >> v[i];
 	}
 
+	sort(v.begin(), v.end());
+
 	for (int i = 0; i < n; i++)
 	{
-		cout << pq.top() << endl;
-		pq.pop();
+		cout << v[i] << endl;
 	}
 
 	return 0;
