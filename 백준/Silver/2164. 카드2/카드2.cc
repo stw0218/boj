2d24713783a67a525718ad8f252c06e52c10af4c
stw0218 <39959351+stw0218@users.noreply.github.com>
2025-02-04 05:47:15 +0900

    [Silver IV] Title: 카드2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.cc
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <stack>
 #include <queue>
+#include <set>
 using namespace std;
 
 #define endl '\n'
@@ -10,21 +16,55 @@ int main()
 	cin.tie(0);
 	cout.tie(0);
 
+	set<int> s;
+	for (int i = 0; i < 20; i++)
+	{
+		int num = 1 << i;
+
+		if (num > 500000)
+			break;
+
+		s.insert(num);
+	}
+
 	int n;
 	cin >> n;
 
-	queue<int> q;
-	for (int i = 1; i <= n; i++)
-		q.push(i);
-
-	while (q.size() > 1)
+	auto it = lower_bound(s.begin(), s.end(), n);
+	if (it != s.end() && *it == n) // 2의 제곱수라면 그대로 출력
 	{
-		q.pop();
-		q.push(q.front());
-		q.pop();
+		cout << n;
+	}
+	else // 2의 제곱수가 아니라면 이전 2의 제곱수와의 차이의 2배를 출력
+	{
+		int prev = *next(it, -1);
+		int diff = n - prev;
+
+		cout << diff * 2;
 	}
 
-	cout << q.front();
+	// 1 -> 1
+	// 2 -> 2
+	// 
+	// 3 -> 2
+	// 4 -> 4
+	// 
+	// 5 -> 2
+	// 6 -> 4
+	// 7 -> 6
+	// 8 -> 8
+	// 
+	// 9 -> 2
+	// 10 -> 4
+	// 11 -> 6
+	// 12 -> 8
+	// 13 -> 10
+	// 14 -> 12
+	// 15 -> 14
+	// 16 -> 16
+	//
+	// 17 -> 2
+
 
 	return 0;
 }
