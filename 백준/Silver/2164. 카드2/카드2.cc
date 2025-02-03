77cf018cf68f44f954e35ef5687915e0c92f87f4
stw0218 <39959351+stw0218@users.noreply.github.com>
2025-02-04 05:38:14 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+#define endl '\n'
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	queue<int> q;
+	for (int i = 1; i <= n; i++)
+		q.push(i);
+
+	while (q.size() > 1)
+	{
+		q.pop();
+		q.push(q.front());
+		q.pop();
+	}
+
+	cout << q.front();
+
+	return 0;
+}
